3b2af84eeb682a424f5a8a3f8f1e50beb8a2a59f
wangqi <wangqihash@163.com>
2019-09-03 23:54:27 +0800

    xx

--- webpack-study-guide/webpack.dev.js
+++ webpack-study-guide/webpack.dev.js
@@ -2,7 +2,7 @@
  * @Description: 
  * @Author: wangqi
  * @Date: 2019-09-02 21:34:02
- * @LastEditTime: 2019-09-02 22:00:27
+ * @LastEditTime: 2019-09-03 23:54:14
  */
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
